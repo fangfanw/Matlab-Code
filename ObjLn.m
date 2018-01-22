67d65ff538d778eb7a0c18a94ba82da38de0d944
fangfanw <30129743+fangfanw@users.noreply.github.com>
2018-01-21 20:12:54 -0600

    Update ObjLn.m

--- ObjLn.m
+++ ObjLn.m
@@ -1,4 +1,14 @@
 function [Lnj, Gradj, Hessj] = ObjLn(Dj, Ytj, covariates)
+% Compute negative misspecified Poisson-log-likelihood function
+% Inputs
+%   Dj : parameters for jth subject; 
+%   Ytj: observations for jth subject, nSamples-by-1
+%   covariates: I-by-nSamples matrix
+%
+% Output: 
+%   Lnj: misspecified Negative Poisson-log-likelihood function for jth subject
+%   Gradj: gradient
+%   Hessj: Hessian matrix
 
 Dj = Dj(:); Ytj = Ytj(:); 
 [nSamples, J] = size(Ytj);
